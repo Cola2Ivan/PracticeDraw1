e593c9feba2d227473c8e045c4462c99268d3832
Cola2Ivan <1164456239@qq.com>
2018-08-02 18:51:16 +0800

    完成直方图练习

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -17,6 +17,10 @@ public class Practice10HistogramView extends View {
 
     private Path mPath = new Path();
 
+    private String[] x_text = new String[]{"Froyo","GB","ICS","JB","KitKat","L","M"};
+
+    private int[] y_percent = new int[]{1, 2, 6, 25, 61, 134, 153};
+
     public Practice10HistogramView(Context context) {
         super(context);
     }
@@ -36,13 +40,31 @@ public class Practice10HistogramView extends View {
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
         mPaint.setColor(Color.WHITE);
-        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
         mPaint.setAntiAlias(true);
         canvas.drawLine(100,50,100,600,mPaint);
-        mPath.moveTo(100,600);
-        mPath.lineTo(7*100 + 8 *10 ,600);
-        canvas.drawLine(100,600,7*100 + 8 *10,600,mPaint);
+//        mPath.moveTo(100,600);
+//        mPath.lineTo(7*100 + 8 *10 ,600);
+        canvas.drawLine(100,600,7*100 + 8 *10 + 150,600,mPaint);
+        mPaint.setTextSize(20);
+        mPaint.setTextAlign(Paint.Align.CENTER);
+        int curX = 100+10;
+        int curY = 600;
+
+        for (int i = 0; i < x_text.length; i++) {
+            mPath.moveTo(curX,curY);
+            mPath.lineTo(curX + 100  ,curY );
+            mPaint.setColor(Color.GREEN);
+            canvas.drawRect(curX, curY - y_percent[i] -5, curX + 100, curY -5, mPaint);
+            mPaint.setColor(Color.WHITE);
+            canvas.drawTextOnPath(x_text[i] , mPath, 0, 30,mPaint);
+
+            mPath.reset();
+            curX = curX + 10 + 100 ;
+        }
+
         mPaint.setTextSize(30);
-        canvas.drawTextOnPath("text",mPath,15,15,mPaint);
+        canvas.drawText("直方图", (6 * 100 +  8 * 10 + 350 ) / 2, 700,mPaint );
+
     }
 }
